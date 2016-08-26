3863d46489946276d1ce3114a0d8f8d9f20047fe
Teacher <Teacher>
2016-08-25 20:05:38 -0400

    implemented row view and responsive design

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,23 +1,37 @@
 
 (function (){
-  angular.module('ngtenants') //getter
-        .controller('MainController', MainController);
+  angular.module('ngtenants') //getter   //geting ngtenants (buildingblocks)
+        .controller('MainController', MainController);  //establishing name for function as well as create new function name MainController
 
 
-  MainController.$inject = ['$scope', 'TenantService'];
+  MainController.$inject = ['$scope', 'TenantService'];  // injecting TenantService factory into maincontroller which injects into DOM using $scope
 
-  function MainController($scope, TenantService){
+  function MainController($scope, TenantService){    //Passing 'actions'? using scope to the DOM ????!?!?!
     $scope.listings = TenantService.listings;
     $scope.create = createListing;
-    $scope.delete = deleteListings;
+    $scope.delete = deleteListing;
+    $scope.edit = editListing;
+    $scope.update = updateListing;
     getListings();
+
+    function editListing(listing){
+      listing.editing = true;
+    }
+
+    function updateListing(listing){
+      listing.editing = false;
+      TenantService.update(listing.id, listing)
+                  .then(function(){
+                    getListings();
+                  });
+    }
 
     function getListings(){
       TenantService.readAll()
                   .then(function(){
                     $scope.listings = TenantService.listings;
                     console.log($scope.listings);
-                  })
+                  });
     }
     function createListing(location, houseOrApt, numberOfRooms, numberOfBathrooms, numberOfRoommates, typeOfLease, arePetsAllowed){
       TenantService.create(location, houseOrApt, numberOfRooms, numberOfBathrooms, numberOfRoommates, typeOfLease, arePetsAllowed)
@@ -33,7 +47,7 @@
                     })
     }
 
-    function deleteListings(id){
+    function deleteListing(id){
       console.log(id);
       TenantService.delete(id)
                     .then(function(){
